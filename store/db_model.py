445dde3514df6cc485ce374775cd0bcaa07b4b5e
Anderzhanov Oleg <anderzanovoleg@gmail.com>
2022-05-01 21:34:17 +0300

    product db has done

--- store/db_model.py
+++ store/db_model.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ARRAY, ForeignKey, JSON
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -9,3 +9,16 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String)
     password = Column(String)
+    products = relationship('Product', backref='user')
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    author = Column(Integer, ForeignKey('users.id'))
+    sex = Column(String)
+    name = Column(String)
+    description = Column(String)
+    sizes = Column(JSON)
+    price = Column(String)
+
